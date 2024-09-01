a0df5c3dee82e4381454446fd15b6d0c048e1001
Rachel Xiao <rachelxiao907@gmail.com>
2024-09-01 12:47:40 -0700

    edit about page

--- static/js/home.js
+++ static/js/home.js
@@ -4,9 +4,9 @@ $(function() {
     strings: [
       "stats rachelxiao<br/>" +
       "><span class='caret'>$</span> job: software engineer intern at nextdoor<br/> ^100" +
-      "><span class='caret'>$</span> education: uc berkeley '26<br/> ^100" +
-      "><span class='caret'>$</span> skills: python, java, c, sql, django, html, css, js<br/> ^100" +
-      "><span class='caret'>$</span> hobbies: traveling, journaling, bowling<br/> ^100"
+      "><span class='caret'>$</span> education: uc berkeley eecs '26<br/> ^100" +
+      "><span class='caret'>$</span> skills: python, java, c, sql, go, django, html, css, js<br/> ^100" +
+      "><span class='caret'>$</span> hobbies: traveling, journaling, bowling,<br/> ^100"
     ],
     showCursor: true,
     cursorChar: '_',
